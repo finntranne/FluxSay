de661f4e7b5f41475f86bd6d5af04c6c5596c841
FinnTran <tranhuuthoai8605@gmail.com>
2026-08-04 14:38:20 +0700

    feat: rate limiting implement

---
+++ backend/src/middleware/arcjet.middleware.js
@@ -0,0 +1,28 @@
+import aj from "../lib/arcjet.js";
+import { isSpoofedBot } from "@arcjet/inspect";
+
+const arcjetProtection = async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req);
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return res
+          .status(429)
+          .json({ message: "Rate limit exceeded. Please try again later" });
+      } else if (decision.reason.isBot()) {
+        return res.status(403).json({ message: "Bot access denied" });
+      } else {
+        return res
+          .status(403)
+          .json({ message: "Access denied by security policy" });
+      }
+    }
+    next();
+  } catch (error) {
+    console.log("Error in Arcjet middleware");
+    next();
+  }
+};
+
+export { arcjetProtection };
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -6,10 +6,15 @@ import {
   logout,
   updateProfile,
 } from "../controllers/auth.controller.js";
+
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 const router = express.Router();
+
+router.use(arcjetProtection);
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.patch("/profile", protectRoute, updateProfile);
+
 export default router;
